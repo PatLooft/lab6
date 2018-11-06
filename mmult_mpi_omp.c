22dab191127e3864143834795fb88ba618a0d145
Patrick D Looft <tug16392@chinook.cis.temple.edu>
2018-11-06 15:21:36 -0500

    test3, stuff from slides

--- mmult_mpi_omp.c
+++ mmult_mpi_omp.c
@@ -22,6 +22,9 @@ int main(int argc, char* argv[])
   // double *bb;	/* the B matrix */
   double *cc1;	/* A x B computed using the omp-mpi code you write */
   double *cc2;	/* A x B computed using the conventional algorithm */
+  double *buffer, ans;
+  double *times;
+  double total_times;
   int run_index;
   int nruns;
   int myid, master, numprocs;
@@ -54,9 +57,9 @@ int main(int argc, char* argv[])
 	}
       }
       ///////////////////////
-      //aa = gen_matrix(nrows, ncols);
-      //bb = gen_matrix(ncols, nrows);
-      //cc1 = malloc(sizeof(double) * nrows * nrows); 
+      aa = gen_matrix(nrows, ncols);
+      bb = gen_matrix(ncols, nrows);
+      cc1 = malloc(sizeof(double) * nrows * nrows); 
       starttime = MPI_Wtime();
       /* Insert your master code here to store the product into cc1 */
       //YOUR CODE GOES HERE
@@ -77,7 +80,7 @@ int main(int argc, char* argv[])
 	anstype = status.MPI_TAG;
 	c[anstype - 1] = ans;
 	if(numsent < nrows){
-	  for(j = 0;; j < ncols; j++){
+	  for(j = 0; j < ncols; j++){
 	    buffer[j] = aa[numsent * ncols + j];
 	  }
 	  MPI_Send(buffer, ncols, MPI_DOUBLE, sender, numsent + 1,
@@ -85,7 +88,7 @@ int main(int argc, char* argv[])
 	  numsent++;
 	}
 	else{
-	  MPI_SEND(MPI_BOTTOM, 0 MPI_DOUBLE, sender, 0, MPI_COMM_WORLD);
+	  MPI_SEND(MPI_BOTTOM, 0, MPI_DOUBLE, sender, 0, MPI_COMM_WORLD);
 	}
       }
       ///////////////////////////////////////////////
@@ -94,20 +97,29 @@ int main(int argc, char* argv[])
       cc2  = malloc(sizeof(double) * nrows * nrows);
       mmult(cc2, aa, nrows, ncols, bb, ncols, nrows);
       compare_matrices(cc2, cc1, nrows, nrows);
-    } else {
+    }
+    else {
       // Slave Code goes here
       MPI_Bcast(b, ncols, MPI_DOUBLE, master,
 		MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-      if(status.MPI_TAG == 0){
-	break;
-      }
-      row = status.MPI_TAG;
-      ans = 0.0;
-#pragma omp shared(ans) for reduction(+:ans)
-      for(j = 0; j < ncols; j++){
-	ans += buffer[j] * b[j];
-      }
-      MPI_Send(&ans, 1, MPI_DOUBLE, master, row, MPI_COMM_WORLD);
+      if(myid <= nrows){
+	while(1){
+	  MPI_Recv(buffer, ncols, MPI_DOUBLE, master,
+		   MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+
+
+      
+	  if(status.MPI_TAG == 0){
+	    break;
+	  }
+	  row = status.MPI_TAG;
+	  ans = 0.0;
+    #pragma omp shared(ans) for reduction(+:ans)
+	  for(j = 0; j < ncols; j++){
+	    ans += buffer[j] * b[j];
+	  }
+	  MPI_Send(&ans, 1, MPI_DOUBLE, master, row, MPI_COMM_WORLD);
+	}
       //////////////////////////////////
     }
   } else {
@@ -117,7 +129,7 @@ int main(int argc, char* argv[])
   return 0;
 }
 
-int mmult_omp(double *c, double *a, int aRows, int aCols,
+int mmult(double *c, double *a, int aRows, int aCols,
 	      double *b, int bRows, int bCols){
   int i, j, k;
 
